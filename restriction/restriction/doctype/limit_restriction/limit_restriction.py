39997f64d51e5cb51989fc557cb27bfc956b20d0
Maison Armani <masonarmani38@gmail.com>
2017-04-05 17:44:39 +0000

    Restriction updates

--- restriction/restriction/doctype/limit_restriction/limit_restriction.py
+++ restriction/restriction/doctype/limit_restriction/limit_restriction.py
@@ -5,35 +5,51 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
-from frappe.utils import  flt
+from frappe.utils import flt,fmt_money
+
+
 class LimitRestriction(Document):
-	pass
-	def validate (self):
-		passed=False
-		field = frappe.db.sql(""" select fieldtype from tabDocField where parent='{}' and fieldname='{}' """
-			.format(self.form,self.currency_field),as_list=1)
-		for row in field:
-			if row[0]=='Currency':
-				passed=True
-		if not passed:
-			frappe.throw("Field is not right!!")
-		#if self.period == 'By Transaction':
-		rule = frappe.db.sql("""select title from `tabLimit Restriction` where disable=0 and form='{}' and currency_field='{}' and period='{}' and user='{}' """
-			.format(self.form,self.currency_field,self.period,self.user),as_list=1)
-		for row in rule:
-			frappe.throw("Duplicate rules found {} ".format(row[0]))
-		
-def check_restriction(doc,method):
-	rule = frappe.db.sql("""select `currency_field`,`limit_value`,period,days,date_field from `tabLimit Restriction` where form='{}' and disable=0 and user='{}' 
-		""".format(doc.doctype,frappe.session.user),as_list=1)
-	for row in rule:
-		if row[2]=="By Transaction" :
-			if flt(doc.get(row[0]))>flt(row[1]):
-				frappe.throw("You Cant Create This document because overlimit maximum alowed transaction is {} ".format(row[1]))
-		else:
-			data = frappe.db.sql("""select sum({}) from `tab{}` where docstatus=1 and ({} between CURDATE() and DATE_SUB(CURDATE(),INTERVAL {} DAY)) """
-				.format(row[0],doc.doctype,row[4],row[3]),as_list=1)
-			for trx in data:
-				if flt(trx[0])+flt(doc.get(row[0]))>flt(row[1]):
-					frappe.throw("You Cant Create This document because overlimit maximum alowed transaction is {} for last {} day".format(row[1],row[3]))
-	
+    def validate(self):
+        passed = False
+        field = frappe.db.sql("""select fieldtype from tabDocField where parent='{}' and fieldname='{}' """
+                              .format(self.form, self.currency_field), as_list=1)
+        for row in field:
+            if row[0] == 'Currency':
+                passed = True
+        if not passed:
+            frappe.throw("Inappropriate field used as currency field.")
+
+
+
+def check_restriction(doc, method):
+    # check if the user has a limitation
+    rule = frappe.db.sql("""select `currency_field`,`limit_value`,`period` ,`days`, `date_field`, `form` ,`user` from `tabLimit Restriction`
+                              where form='{}' and disable=0 and user='{}'""".format(doc.doctype, frappe.session.user),
+                         as_list=1)
+    for row in rule:
+        """
+            row[0] holds the currency field used in the doctype / form
+            row[1] holds the limit value
+            row[2] holds the restriction type
+            row[3] holds the number of days restriction applies if the restriction type is per day
+            row[4] holds the date field used in the doctype / form
+            row[5] holds the doctype / form
+            row[6] holds the user the limitation is on doctype / form
+        """
+        if row[2] == "By Transaction":
+            if flt(doc.get(row[0])) > flt(row[1]):
+                frappe.throw("Sorry, You can not create this document because {} is above your maximum transaction limit of {} pay transaction".
+                             format(fmt_money(flt(doc.get(row[0]))), fmt_money(row[1]) ))
+        else:
+            qry = """select sum({}) from `tab{}` where docstatus=1
+                                    and (DATE(`{}`) between DATE_SUB(CURDATE(),INTERVAL {} DAY) and CURDATE()) and modified_by='{}'""".format(
+                row[0], row[5], row[4], row[3], row[6])
+            data = frappe.db.sql(qry, as_list=0)
+            frappe.errprint(qry)
+
+            for transaction in data:
+                if flt(transaction[0]) + flt(doc.get(row[0])) > flt(row[1]):
+                    if row[3] == 1 : day = "a day"
+                    else: day = str(row[3])+ " days"
+                    frappe.throw("Sorry, You can not create this document because {} is above your maximum transaction limit of {} in {}"
+                                 .format(fmt_money(flt(transaction[0]) + flt(doc.get(row[0]))), fmt_money(row[1]),day) )
