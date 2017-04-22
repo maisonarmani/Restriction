cd414bf6e52b84fbfafc19cee2e3d669c69ff608
mason armani <masonarmani38@gmail.com>
2017-04-22 12:10:45 +0000

    Latest Update for limit restriction

--- restriction/restriction/doctype/limit_restriction/limit_restriction.js
+++ restriction/restriction/doctype/limit_restriction/limit_restriction.js
@@ -1,8 +1,15 @@
-// Copyright (c) 2016, bobzz.zone@gmail.com and contributors
+// Copyright (c) 2016, bobzz.zone@gmail.com and masonarmani38@gmail.com contributors
 // For license information, please see license.txt
-
+// Limit Restriction
 frappe.ui.form.on('Limit Restriction', {
 	refresh: function(frm) {
 
 	}
 });
+
+/**
+ *
+	cur_frm.fields_dict['currency_field'].get_query = function(doc) {
+		return { filters:{ 'parent': doc.form } }
+	};
+ */
